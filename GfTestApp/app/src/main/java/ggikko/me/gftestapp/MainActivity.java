6c1aa0b3610b135b8e4fd186fb622a7d9734c1aa
Jihong Park <ggikko@hotmail.com>
2016-07-03 01:21:17 +0900

    add comment

--- GfTestApp/app/src/main/java/ggikko/me/gftestapp/MainActivity.java
+++ GfTestApp/app/src/main/java/ggikko/me/gftestapp/MainActivity.java
@@ -3,6 +3,7 @@ package ggikko.me.gftestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//ggikko
 public class MainActivity extends AppCompatActivity {
 
     @Override
